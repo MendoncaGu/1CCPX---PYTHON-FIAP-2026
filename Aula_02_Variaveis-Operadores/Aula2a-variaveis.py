ad3cb996474436b478ad67f67ab8f153e6f4f990
Mendoncagu <gugamendocaduarte2@gmail.com>
2026-03-19 22:28:48 -0300

    Aula 2 variaveis e o operadores

---
+++ Aula_02_Variaveis-Operadores/Aula2a-variaveis.py
@@ -0,0 +1,36 @@
+from numba.core.ir import Print
+
+print("hello wolrd")
+
+print(7+4)
+print("7+4")
+print("7"+"4")
+
+#Comentario de uma linha
+
+'''
+Mais de uma linha
+'''
+
+# Variaveis
+
+nome = "Gustavo"
+idade = 19
+peso = 80
+
+print(f"Olá, sou {nome} tenho {idade} e peso {peso}")
+
+#IMPUTS - Simulação de formulario
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade + 1
+
+print(f"Olá {nome}")
+print(nova_idade + 1)
+'''
+-------------------------------------------------------------------------------
+'''
+
+
